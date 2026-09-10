1ecc71309742d3f5a81738b763a1ef98f607c85d
ahmedrazaofficial <syedahmedrazabukhari15@gmail.com>
2026-09-10 22:01:53 +0500

    Function test done

---
+++ functions/test.js
@@ -0,0 +1,65 @@
+//1.
+function printName(name) {
+    // console.log(name);
+}
+printName("ahmed")
+printName("ahmed")
+printName("ahmed")
+
+//2.
+function addTwoNumber(num1, num2) {
+    return num1 + num2
+}
+console.log(addTwoNumber(10, 20));
+
+
+//3.
+function twoNumberReceive(number1, number2) {
+    return number1 * number2
+
+}
+console.log(twoNumberReceive(10, 20))
+
+//4.
+function squareNumber(square1) {
+    return square1 * square1
+
+}
+console.log(squareNumber(22))
+
+//5.
+
+
+
+//6.
+function userName(username) {
+    return `${username} just logged in `
+
+}
+console.log(userName("Ahmed"))
+
+
+//7.
+function sumThreeNumber(num1, num2, num3) {
+    return num1 + num2 + num3
+
+}
+console.log(sumThreeNumber(2, 2, 2))
+
+//8.
+
+
+
+//9.
+function totalBill(price, quantity) {
+    return price * quantity
+}
+console.log(totalBill(1200, 4))
+
+
+//10.
+function nameAge(name, age) {
+    return `${name} is ${age} years old`
+}
+
+console.log(nameAge("ahmed", 20))
